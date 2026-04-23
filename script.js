875dc00d94a142f4a0c7cc46846536571c7c80b8
Rayhan Ahmed <rmd09181@gmail.com>
2026-04-23 22:05:02 +0600

    store all the elements in varilable

---
+++ script.js
@@ -0,0 +1,5 @@
+const balance = document.querySelector("#balance");
+const incomeTitle = document.querySelector("#incomeTitle");
+const incomeAmount = document.querySelector("#incomeAmount");
+const addIncomeBtn = document.querySelector("#addIncomeBtn");
+const transactionList = document.querySelector("#transactionList");
