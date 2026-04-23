c8a1d3f6b8272fac40e6c82dbe5f570ef3a6a6b5
Rayhan Ahmed <rmd09181@gmail.com>
2026-04-23 22:18:30 +0600

    add localstorage

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const incomeTitle = document.querySelector("#incomeTitle");
 const incomeAmount = document.querySelector("#incomeAmount");
 const addIncomeBtn = document.querySelector("#addIncomeBtn");
 const transactionList = document.querySelector("#transactionList");
+
+let transactions = JSON.parse(localStorage.setItem("transactions")) || []
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Local storage function
+function updateLocalStorage() {
+    localStorage.getItem("transactions",JSON.stringify(transactions));
+}
